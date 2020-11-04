5d23ffb2627897ca5bdff51bb694754c259d4a43
Dirk Lehmann <develop@dj-l.de>
2020-11-04 03:15:09 +0100

    CI: Minor fix during testing for zero arguments ...

--- .github/actions/cc65+vice-environment/entrypoint.sh
+++ .github/actions/cc65+vice-environment/entrypoint.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-if [ -z $@ ]; then
+if [ -z "$@" ]; then
   echo "$0: No command to execute given!" >&2;
   exit 1;
 fi
